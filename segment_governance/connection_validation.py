cf6274d286046e08bef3af5261b3a62f845faf0d
RICHARD GRIFFITHS <griff182uk@yahoo.co.uk>
2022-04-10 11:31:33 +0100

    added get destination call

--- segment_governance/connection_validation.py
+++ segment_governance/connection_validation.py
@@ -3,56 +3,65 @@ from logging import raiseExceptions
 import requests
 import json
 import re
+import os
 
-url = "https://platform.segmentapis.com/v1beta/workspaces/googlemail-griff182uk/sources"
+workspace = "googlemail-griff182uk"
+team_names_list = ["griffteam"]
 
-payload={}
+baseUrl = "https://platform.segmentapis.com/v1beta"
+sourcesUrl = f"{baseUrl}/workspaces/{workspace}/sources"
+
+source_payload={}
+bearer_token =  os.getenv('SEGMENT_TOKEN')
 headers = {
-  'Authorization': 'Bearer ...',
+  'Authorization': f'Bearer {bearer_token}',
   'Content-Type': 'application/json'
 }
 
-response = requests.request("GET", url, headers=headers, data=payload)
-payload = response.json()
-dump = json.dumps(payload, indent=4)
+source_response = requests.request("GET", sourcesUrl, headers=headers, data=source_payload)
+source_payload = source_response.json()
+##dump = json.dumps(payload, indent=4) ##for debugging
 
-sources_list = (payload["sources"])
-team_names_list = ["griffteam"]
+sources_list = (source_payload["sources"])
 
 for source in sources_list:
-  source_name = source["display_name"]
+  source_display_name = source["display_name"]
+  source_name = source["name"]
+  destinationsUrl = f"{baseUrl}/{source_name}/destinations"
+  destinations_response = requests.request("GET", destinationsUrl, headers=headers)
+  destinations_list = destinations_response.json()
 
-  test_result = re.search("(dev|uat|prd)_(griffco|dogadopt)_(blog|website|demo)", source_name)
+  test_result = re.search("(dev|uat|prd)_(griffco|dogadopt)_(blog|website|demo)", source_display_name)
   if(test_result is None):
-    print(f"Fail: Source name {source_name} does not meet naming standard policy.")
+    print(f"Fail: Source name {source_display_name} does not meet naming standard policy.")
     break
   else:
-    print(f"Success: Source name {source_name} does meet naming standard policy.")
+    print(f"Success: Source name {source_display_name} does meet naming standard policy.")
 
   try:
-    environment_nc, brand_nc, source_nc = source_name.split("_")
+    environment_nc, brand_nc, source_nc = source_display_name.split("_")
   except Exception as e:
-    print(f"Fail: Source name {source_name} does not meet naming standard policy.")
+    print(f"Fail: Source name {source_display_name} does not meet naming standard policy.")
 
   try:
     environment_label = source["labels"]["environment"]
   except Exception as e:
-    print(f"Fail: Source name {source_name} does not have an environment label.")
+    print(f"Fail: Source name {source_display_name} does not have an environment label.")
     break
 
   if(environment_label  == environment_nc):
-    print(f"Success: Source name {source_name} environment {environment_label} is an expected environment label.")
+    print(f"Success: Source name {source_display_name} environment {environment_label} is an expected environment label.")
   else:
-    print(f"Fail: Source name {source_name} environment {environment_label} is not expected environment label.")
+    print(f"Fail: Source name {source_display_name} environment {environment_label} is not expected environment label.")
 
   try:
     team_label = source["labels"]["team"]
   except Exception as e:
-    print(f"Fail: Source name {source_name} does not have a team label.")
+    print(f"Fail: Source name {source_display_name} does not have a team label.")
     break
 
   if(team_label  in (team_names_list)):
-    print(f"Success: Source name {source_name} team {team_label} is an expected team label.")
+    print(f"Success: Source name {source_display_name} team {team_label} is an expected team label.")
   else:
-    print(f"Fail: Source name {source_name} team {team_label} is not expected team label.")
+    print(f"Fail: Source name {source_display_name} team {team_label} is not expected team label.")
 
